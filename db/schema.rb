f3fbe6c95adb278e79026cc2e4680ca3dc824656
TikhonovAndrew <itoffice@azeristudent.az>
2018-01-31 12:02:47 +0000

    index updated

--- db/schema.rb
+++ db/schema.rb
@@ -156,9 +156,6 @@ ActiveRecord::Schema.define(version: 20180103011657) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "surname"
-    t.string   "phone"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
